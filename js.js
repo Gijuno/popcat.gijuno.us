169251f1995cedd8b5d5810930138cd317887ed7
Gijuno <46197097+Gijuno@users.noreply.github.com>
2021-01-25 00:48:58 +0900

    Update js.js

--- js.js
+++ js.js
@@ -1,16 +1,16 @@
 var count = 1;
 window.onload = function(){
     var hw = document.getElementById('hw');
-    var sound = new Audio("sound.mp3");
+    var sound = new Audio("./sound.mp3");
     hw.addEventListener('click', function(){
         sound.play();
         if (count == 1) {
             var imgTag = document.getElementById("photo");
-            imgTag.setAttribute("src","popcat_2.jpeg");
+            imgTag.setAttribute("src","./popcat_2.jpeg");
             count = 2;
         } else if (count == 2) {
             var imgTag = document.getElementById("photo");
-            imgTag.setAttribute("src","popcat_1.jpeg");
+            imgTag.setAttribute("src","./popcat_1.jpeg");
             count = 1;
         }
     })
